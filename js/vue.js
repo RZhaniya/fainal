caad156ec305027ccdeb181f8acd61189462e931
RZhaniya <81968879+RZhaniya@users.noreply.github.com>
2021-12-12 21:20:33 +0600

    Add files via upload

---
+++ js/vue.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#feedback',
+    data: {
+      listOfFeedbacks:[
+          {title:"1Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis,fugiat labore. Veritatis et omnis consequatur."
+        , author:"1Will Smit"},
+        {title:"2Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis,fugiat labore. Veritatis et omnis consequatur."
+        , author:"2Will Smit"},
+        {title:"3Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis,fugiat labore. Veritatis et omnis consequatur."
+        , author:"3Will Smit"}
+      ]
+     
+    }
+  })
